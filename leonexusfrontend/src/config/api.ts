32980df583d9fe2048c5ab0b9d1e24a520d7a201
Leone Munyao <leonemunyao@gmail.com>
2025-09-30 00:46:19 +0300

    Intergrating the frontend and the django backend.

--- leonexusfrontend/src/config/api.ts
+++ leonexusfrontend/src/config/api.ts
@@ -1,11 +1,5 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://alx-project-nexus-3ow0.onrender.com/api',
+  BASE_URL: import.meta.env.VITE_API_BASE_URL,
   TIMEOUT: 10000, // 10 seconds
 };
-
-// For development, you can override the base URL here
-// export const API_CONFIG = {
-//   BASE_URL: 'http://localhost:8000/api',
-//   TIMEOUT: 10000,
-// };
