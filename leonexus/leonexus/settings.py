726c28cbe416b509d9c01dca04f77c01d9eeccab
Leone Munyao <leonemunyao@gmail.com>
2025-09-30 00:50:56 +0300

    Intergrating the frontend and the django backend.

--- leonexus/leonexus/settings.py
+++ leonexus/leonexus/settings.py
@@ -176,7 +176,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-    "https://leonexus.vercel.app", 
+    "https://alx-project-nexus-leone.vercel.app/", 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
